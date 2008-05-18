59224055f8cc06f9ac87bfc787ba5271375ffa61
pwnd <arandes@nzr.(none)>
2008-05-18 22:14:27 +0200

    Added header file for File namespace.

---
+++ src/File/File.h
@@ -0,0 +1,46 @@
+// LICENSE/*{{{*/
+/*
+  Copyright (C) 2008 Andreas Waidler
+
+  This program is free software: you can redistribute it and/or modify it 
+  under the terms of the GNU General Public License as published by the Free 
+  Software Foundation, either version 3 of the License, or (at your option) 
+  any later version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT 
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for 
+  more details.
+
+  You should have received a copy of the GNU General Public License along with 
+  this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+/*}}}*/
+
+/**
+ * @file File.h
+ * @brief File for the File namespace.
+ * @author Andreas Waidler
+ * @date 2008-05-18
+ */
+
+/* $Id$ */
+
+#ifndef FILE_FILE_H
+#define FILE_FILE_H
+
+/**
+ * @namespace File
+ * @brief Contains the abstract base classes for file operations.
+ */
+
+namespace File
+{
+    class Input;
+    class Output;
+}
+
+#endif // FILE_FILE_H
+
+// Use no tabs at all; four spaces indentation; max. eighty chars per line.
+// vim: et ts=4 sw=4 tw=80 fo+=c fdm=marker
