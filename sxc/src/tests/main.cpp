47d9f71e000dd9adbfa6fec42e1462ed22023c88
r0q <deen@chiaki.minami>
2008-07-17 17:12:47 +0200

    Return code

---
+++ sxc/src/tests/main.cpp
@@ -0,0 +1,12 @@
+#include <cppunit/ui/text/TestRunner.h>
+
+#include "OptionTest.h"
+
+int main(int argc, char **argv)
+{
+    CppUnit::TextUi::TestRunner runner;
+
+    runner.addTest(OptionTest::suite());
+
+    return !runner.run();
+}
