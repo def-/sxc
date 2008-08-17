30b8cfa3adcab827fe420852e75703f6148fb4b0
pwnd <ptr@nzr.(none)>
2008-08-17 17:52:19 +0200

    Wrote Exception::errnoToType().

--- sxc/src/Exception/Errno.cxx
+++ sxc/src/Exception/Errno.cxx
@@ -18,23 +18,59 @@
  */
 /*}}}*/
 
-/* $Id$ */
-
 // INCLUDES/*{{{*/
+
+#include <errno.h>
 
 #include <Exception/Errno.hxx>
 #include <Exception/Type.hxx>
 
 /*}}}*/
 
-Exception::Type Exception::errnoToType(int p_errno)
+namespace Exception
 {
-    // TODO
-    return General;
+    Type errnoToType(int p_errno)
+    {
+        Type result = General;
+
+        switch (p_errno) {
+        case EACCES:
+            result = AccessDenied;
+            break;
+
+        case EEXIST:
+            result = FileExists;
+            break;
+
+        case ENOENT:
+            result = FileMissing;
+            break;
+
+        case ELOOP:
+        case ENOTDIR:
+            result = BadPath;
+            break;
+
+        case ENAMETOOLONG:
+            result = BadFile;
+            break;
+
+        case ENOSPC:
+        case EROFS:
+            result = WriteFailed;
+            break;
+        // The following ones should not be needed but may be passed as
+        // parameter and could be handled in future versions:
+        // case EBADF:
+        // case EFAULT:
+        // case ENOMEM:
+        }
+
+        return result;
+    }
 }
 
 
 // Use no tabs at all; four spaces indentation; max. eighty chars per line.
 // vim: et ts=4 sw=4 tw=80 fo+=c fdm=marker
-
 
