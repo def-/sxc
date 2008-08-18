d96d9c4019c64e782deacc82f62794de6bb06108
pwnd <ptr@nzr.(none)>
2008-08-18 17:06:06 +0200

    Updated autotool-files to support tests/File dir.

--- sxc/tests/testRegistry.cxx
+++ sxc/tests/testRegistry.cxx
@@ -25,11 +25,14 @@
 #include <cppunit/extensions/HelperMacros.h>
 
 #include <Option/OptionTest.hxx>
+#include <File/TestInputDummy.hxx>
 
 /*}}}*/
 
 // Registering all unit tests here, to make them easier to disable and enable.
 
 CPPUNIT_TEST_SUITE_REGISTRATION(OptionTest);
+CPPUNIT_TEST_SUITE_REGISTRATION(TestInputDummy);
+
 // Use no tabs at all; four spaces indentation; max. eighty chars per line.
 // vim: et ts=4 sw=4 tw=80 fo+=c fdm=marker
