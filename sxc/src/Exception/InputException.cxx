016521374ea14dd139a67f82ff94996cb18c4546
pwnd <ptr@nzr.(none)>
2008-08-25 12:21:13 +0200

    Added Exception::InputException.

---
+++ sxc/src/Exception/InputException.cxx
@@ -0,0 +1,56 @@
+// LICENSE/*{{{*/
+/*
+  sxc - Simple Xmpp Client
+  Copyright (C) 2008 Dennis Felsing, Andreas Waidler
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*}}}*/
+
+// INCLUDES/*{{{*/
+
+#include <sstream>
+
+#include <Exception/InputException.hxx>
+
+/*}}}*/
+
+namespace Exception
+{
+    InputException::InputException(Type type, std::string &message)/*{{{*/
+    : Exception(type, message)
+    {
+    }
+
+/*}}}*/
+    InputException::InputException(Type type, const char *message)/*{{{*/
+    : Exception(type, message)
+    {
+    }
+
+/*}}}*/
+    void InputException::createDescription() throw()/*{{{*/
+    {
+        std::stringstream desc;
+        desc << "InputException: " << _message << " (" << _type << ") .";
+        _description = desc.str();
+    }
+
+/*}}}*/
+}
+
+
+// Use no tabs at all; four spaces indentation; max. eighty chars per line.
+// vim: et ts=4 sw=4 tw=80 fo+=c fdm=marker
+
