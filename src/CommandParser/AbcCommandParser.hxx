55f80973576525e10b32e8dc90c5fb13f79a82c4
pwnd <ptr@nzr.(none)>
2008-08-29 14:34:43 +0200

    src/CommandParser/AbcCommandParser.hxx: Added docblocks.

--- src/CommandParser/AbcCommandParser.hxx
+++ src/CommandParser/AbcCommandParser.hxx
@@ -29,43 +29,232 @@
 
 /*}}}*/
 
-/**
- * @brief DATA LOSS ZOMFGOLOLOL
- * @author Andreas Waidler
- *
- */
 namespace CommandParser
 {
+    // typedef std::pair<unsigned short, bool> param;/*{{{*/
+
+    /**
+     * @brief A parameter specification.
+     *
+     * Binds together the amount of mandatory parameters of a command and
+     * whether the command may take a last optional parameter.
+     */
     typedef std::pair<unsigned short, bool> param;
+
+/*}}}*/
+    // typedef std::pair<std::string, param> command;/*{{{*/
+
+    /**
+     * @brief A command and its parameter specification.
+     *
+     * Binds the command name and its parameter specification together.
+     */
     typedef std::pair<std::string, param> command;
+
+/*}}}*/
+    // typedef std::map<std::string, param> commandMap;/*{{{*/
+
+    /**
+     * @brief Contains commands and their parameter specifications.
+     *
+     * Maps command names to their parameter specifications.
+     */
     typedef std::map<std::string, param> commandMap;
 
+/*}}}*/
+    /**
+     * @brief Abstract base class for command parsing.
+     *
+     * Child classes only have to implement the abstract method @ref
+     * _createCommands(). Assume, that @ref _createCommands() returns a useable
+     * @c commandMap, the parser will work out of the box.
+     *
+     * You might want to give derived classes a method to handle the parsed
+     * command. This method could simply call @ref parse() and use the container
+     * returned by @ref getParsed() to decide what to do.
+     *
+     * @author Andreas Waidler
+     */
     class AbcCommandParser
     {
         public:
+            // AbcCommandParser(const std::string &command);/*{{{*/
+
+            /**
+             * @brief Default constructor.
+             *
+             * Stores @a command to be parsed at a later moment.
+             *
+             * @param command A potential command to be parsed.
+             */
             AbcCommandParser(const std::string &command);
+
+/*}}}*/
+            // virtual ~AbcCommandParser();/*{{{*/
+
+            /// Virtual destructor, does no work currently.
             virtual ~AbcCommandParser();
+
+/*}}}*/
+            // void parse();/*{{{*/
+
+            /**
+             * @brief Parses the command.
+             *
+             * The results will be stored and can be obtained by calling @ref
+             * getName(), @ref getParameterString() or @ref getParsed().
+             *
+             * Assumes that the command is valid and that it has not been parsed
+             * yet. If any of that was a mistake an exception will be thrown.
+             *
+             * Internals: The command is split by seperating the parameters by
+             * a space (' '). The parameter list will be sliced until the amount
+             * of mandatory parameters minus one is reached. If the command is
+             * overloadable this method tries to slice the remaining part of the
+             * list again. Everything remaining in the parameter list is now
+             * considered to be the last parameter, thus ignoring any
+             * whitespaces in it.
+             *
+             * @exception InputException(InvalidUsage) If already parsed.
+             * @exception InputException(NoCommand) When command prefix not
+             *            found.
+             * @exception InputException(InvalidCommand) When the command does
+             *            not match any specifications returned by
+             *            @ref _createCommands().
+             * @see getParsed() For information how to use the parsed command.
+             */
             void parse();
+
+/*}}}*/
+            // const std::string &get() const;/*{{{*/
+
+            /**
+             * @brief Returns a constant reference of the command.
+             * @return Constant reference of the command that has been passed to
+             *         the constructor.
+             */
             const std::string &get() const;
+
+/*}}}*/
+            // const std::string &getName() const;/*{{{*/
+
+            /**
+             * @brief Returns a reference of the name of the command.
+             * @exception InputException(InvalidUsage) If not parsed.
+             * @return Reference of the name of the command.
+             */
             const std::string &getName() const;
+
+/*}}}*/
+            // const std::string &getParameterString() const;/*{{{*/
+
+            /**
+             * @brief Returns a constant reference to all parameters
+             * @exception InputException(InvalidUsage) If not parsed.
+             * @return Constant reference to a string containing all parameters.
+             */
             const std::string &getParameterString() const;
+
+/*}}}*/
+            // const std::deque<std::string> &getParsed() const;/*{{{*/
+
+            /**
+             * @brief Returns a constant container with the parsed command.
+             *
+             * The container returned will contain the name of the command as
+             * first element (@ref getName()). The following elements will be
+             * the contents of the parameter list (@ref getParameterString()),
+             * seperated by a space (' ') until the number of arguments is
+             * reached. The last element will contain everything from the
+             * beginning of the last parameter to the end of the parameter list,
+             * thus ignoring any whitespaces. If the command is specified as
+             * overloadable and the "last parameter" contains an additional
+             * space, it will be sliced again.
+             *
+             * @return Constant double-ended queue containing strings, each
+             *         having the value of a space-seperated part of the command
+             */
             const std::deque<std::string> &getParsed() const;
 
+/*}}}*/
+
         protected:
+            // const commandMap &_getCommands() const;/*{{{*/
+
+            /**
+             * @brief Returns a constant reference to all valid commands.
+             *
+             * If the @c commandMap @ref _commands has not been set (i.e. it is
+             * empty) the abstract method @ref _createCommands() is called and
+             * the resulting @c commandMap is stored in @ref _commands. This
+             * method then returns a constant reference to it.
+             * 
+             * @return Constant reference to the result of @ref _createCommands()
+             */
             const commandMap &_getCommands() const;
+
+/*}}}*/
+            // static const char _commandPrefix = ':';/*{{{*/
+
+            /// Prefix for all commands.
             static const char _commandPrefix = ':';
 
+/*}}}*/
+
         private:
+            // virtual commandMap _createCommands() const = 0;/*{{{*/
+
+            /**
+             * @brief Creates a container holding all possible commands.
+             *
+             * @note Pure virtual, aka abstract.
+             * 
+             * @return All valid commands and the specification of their
+             *         parameters
+             */
             virtual commandMap _createCommands() const = 0;
 
-            /// Never access directly!
+/*}}}*/
+            // static commandMap _commands;/*{{{*/
+
+            /**
+             * @brief Holds all valid commands.
+             * Will be set by the abstract method @ref _createCommands().
+             * @warning Never access directly, use @ref _getCommands().
+             */
             static commandMap _commands;
 
+/*}}}*/
+            // const std::string &_command;/*{{{*/
+
+            /// The command to parse which has been passed to the constructor.
             const std::string &_command;
+
+/*}}}*/
+            // std::string _commandName;/*{{{*/
+
+            /// The name of the command, will be set by @ref parse().
             std::string _commandName;
+
+/*}}}*/
+            // std::string _parameters;/*{{{*/
+
+            /// The parameter list of the command, set by @ref parse()
             std::string _parameters;
+
+/*}}}*/
+            // std::deque<std::string> _parsed;/*{{{*/
+
+            /// Results of @ref parse(), see that docblock for information.
             std::deque<std::string> _parsed;
+
+/*}}}*/
+            // bool _isParsed;/*{{{*/
+
+            /// Indicates whether @ref parse() did return successfully.
             bool _isParsed;
+
+/*}}}*/
     };
 }
 
