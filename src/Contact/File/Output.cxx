aff3c363c39da6d2a349ca061d41bdc8b9a88048
pwnd <ptr@err.(none)>
2008-10-02 14:17:50 +0200

    Added Contact::File::Output

---
+++ src/Contact/File/Output.cxx
@@ -0,0 +1,68 @@
+// LICENSE/*{{{*/
+/*
+  sxc - Simple Xmpp Client
+  Copyright (C) 2008 Dennis Felsing, Andreas Waidler
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*}}}*/
+
+// INCLUDES/*{{{*/
+
+#ifdef HAVE_CONFIG_H
+#   include <config.hxx>
+#   include <print.hxx>
+#endif
+
+#include <string>
+#include <Control/Control.hxx>
+#include <Contact/Contact.hxx>
+#include <Contact/File/Output.hxx>
+#include <Time/Timestamp.hxx>
+#include <Time/LocalDateTime.hxx>
+#include <Time/IsoDateTimeFormat.hxx>
+
+/*}}}*/
+
+namespace Contact
+{
+    namespace File
+    {
+        Output::Output(Control::Control &control, Contact &contact)/*{{{*/
+        : _control(&control)
+        , _contact(&contact)
+        {
+        }
+
+/*}}}*/
+        std::string Output::_createPath() const/*{{{*/
+        {
+            return _control->getJid().bare() + '/'
+                   + _contact->getJid().bare() + "/out";
+        }
+
+/*}}}*/
+        std::string Output::_format(const std::string &output) const/*{{{*/
+        {
+            Time::LocalDateTime date = Time::LocalDateTime(Time::Timestamp());
+            Time::IsoDateTimeFormat format(&date);
+            return format.string() + ' ' + output;
+        }
+
+/*}}}*/
+    }
+}
+
+// Use no tabs at all; four spaces indentation; max. eighty chars per line.
+// vim: et ts=4 sw=4 tw=80 fo+=c fdm=marker
---
+++ src/Contact/File/Output.hxx
@@ -0,0 +1,104 @@
+// LICENSE/*{{{*/
+/*
+  sxc - Simple Xmpp Client
+  Copyright (C) 2008 Dennis Felsing, Andreas Waidler
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*}}}*/
+
+#ifndef CONTROL_FILE_OUTPUT_HXX
+#define CONTROL_FILE_OUTPUT_HXX
+
+// INCLUDES/*{{{*/
+
+#include <string>
+#include <File/AbcOutput.hxx>
+#include <Control/Control.hxx>
+#include <Contact/Contact.hxx>
+
+/*}}}*/
+
+namespace Contact
+{
+    namespace File
+    {
+        /**
+         * @brief Output class for contacts.
+         *
+         * Creates the file $JID/$CONTACT/out which contains messages from and 
+         * to that contact.
+         *
+         * @author Andreas Waidler
+         */
+        class Output : public ::File::AbcOutput
+        {
+            public:
+                // Output(Control::Control &control, Contact &contact);/*{{{*/
+
+                /**
+                 * @brief Initializes the object.
+                 *
+                 * @param control Central object, holding account information
+                 * @param contact Contact to which this output belongs
+                 */
+                Output(Control::Control &control, Contact &contact);
+
+/*}}}*/
+
+            private:
+                // Control::Control *_control;/*{{{*/
+
+                /// Central control object, used for error handling or getting 
+                /// account information.
+                Control::Control *_control;
+
+/*}}}*/
+                // Contact *_contact;/*{{{*/
+
+                /// Contact to which this output belongs.
+                Contact *_contact;
+
+/*}}}*/
+                // std::string _createPath() const;/*{{{*/
+
+                /**
+                 * @brief Creates a string containing the path of this file.
+                 *
+                 * @see File::AbcOutput::_createPath()
+                 *
+                 * @return $accountName/out
+                 */
+                std::string _createPath() const;
+
+/*}}}*/
+                // std::string _format(const std::string &output) const;/*{{{*/
+
+                /**
+                 * @brief Formats the output.
+                 *
+                 * @param output String to be formatted.
+                 * @return A formatted string.
+                 */
+                std::string _format(const std::string &output) const;
+
+/*}}}*/
+        };
+    }
+}
+
+#endif // CONTROL_FILE_OUTPUT_HXX
+
+// Use no tabs at all; four spaces indentation; max. eighty chars per line.
+// vim: et ts=4 sw=4 tw=80 fo+=c fdm=marker
